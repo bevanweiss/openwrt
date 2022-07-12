54212f671a5e3d1cff96a5cae3fe696e7bc04b4b
Birger Koblitz <git@birger-koblitz.de>
2022-07-12 20:46:13 +0200

    realtek: Improve debugging information to aid development
    Print beter debugging information to help with diagnosing problems with
    the newly introduced RTL8226 work.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- target/linux/realtek/files-5.15/drivers/net/ethernet/rtl838x_eth.c
+++ target/linux/realtek/files-5.15/drivers/net/ethernet/rtl838x_eth.c
@@ -2519,7 +2519,6 @@ static int rtl838x_mdio_init(struct rtl838x_eth_priv *priv)
 	u32 pn;
 	int ret;
 
-	pr_debug("%s called\n", __func__);
 	mii_np = of_get_child_by_name(priv->pdev->dev.of_node, "mdio-bus");
 
 	if (!mii_np) {
@@ -2595,9 +2594,6 @@ static int rtl838x_mdio_init(struct rtl838x_eth_priv *priv)
 
 		if (of_property_read_u32(dn, "sds", &priv->sds_id[pn]))
 			priv->sds_id[pn] = -1;
-		else {
-			pr_info("set sds port %d to %d\n", pn, priv->sds_id[pn]);
-		}
 
 		if (of_property_read_u32_array(dn, "rtl9300,smi-address", &smi_addr[0], 2)) {
 			/* Integrated PHYs associated to a SerDes do not have an smi_bus */
@@ -2637,9 +2633,7 @@ static int rtl838x_mdio_init(struct rtl838x_eth_priv *priv)
 			continue;
 		if (of_get_phy_mode(dn, &priv->interfaces[pn]))
 			priv->interfaces[pn] = PHY_INTERFACE_MODE_NA;
-		pr_debug("%s phy mode of port %d is %s\n", __func__, pn, phy_modes(priv->interfaces[pn]));
 	}
-
 	snprintf(priv->mii_bus->id, MII_BUS_ID_SIZE, "%pOFn", mii_np);
 	ret = of_mdiobus_register(priv->mii_bus, mii_np);
 
