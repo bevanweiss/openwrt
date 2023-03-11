8358a5adcbe3ed7e8b659136fc285452ba3b1cde
Bevan Weiss <bevan.weiss@gmail.com>
2023-03-11 20:35:54 +1100

    Add Hasivo S1100WP-8GT-SE rtl9303 based board

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -1,4 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-only
+define Device/hasivo_s1100wp-8gt-se
+  SOC := rtl9303
+  DEVICE_VENDOR := Hasivo
+  DEVICE_MODEL := S1100WP-8GT-SE
+  IMAGE_SIZE := 8364k
+  KERNEL_INITRAMFS := \
+	kernel-bin | \
+	append-dtb | \
+	gzip | \
+	uImage gzip
+endef
+TARGET_DEVICES += hasivo_s1100wp-8gt-se
 
 define Device/zyxel_xgs1250-12
   SOC := rtl9302
