31228a9868cfdec0cbd348bf4cecc55e9bd38141
Bevan Weiss <bevan.weiss@gmail.com>
2026-04-12 20:31:45 +1000

    realtek: Add hasivo fw1100wp-4xs-4xgt
    Yet another Hasivo switch
    
    Signed-off-by: Bevan Weiss <bevan.weiss@gmail.com>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -12,6 +12,15 @@ define Device/d-link_dgs-1250-28x
 endef
 TARGET_DEVICES += d-link_dgs-1250-28x
 
+define Device/hasivo_fw1100wp-4xs-4xgt
+  SOC := rtl9303
+  DEVICE_VENDOR := Hasivo
+  DEVICE_MODEL := FW1100WP-4XS-4XGT
+  IMAGE_SIZE := 12288k
+  $(Device/kernel-lzma)
+endef
+TARGET_DEVICES += hasivo_fw1100wp-4xs-4xgt
+
 define Device/hasivo_s1100w-8xgt-se
   SOC := rtl9303
   DEVICE_VENDOR := Hasivo
